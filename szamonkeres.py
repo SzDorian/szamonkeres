a7a8d62bd5af56589324fb9af4f3b7407a661169
Szabó Dorián <SzaboDo@KKSZKI.LOCAL>
2022-12-01 13:07:35 +0100

    Python

---
+++ szamonkeres.py
@@ -0,0 +1,27 @@
+#1.feladat
+'''
+vezetek_nev = (input('Vezetéknév:'))
+kereszt_nev = (input('Keresztnév:'))
+nev1 = vezetek_nev+kereszt_nev
+nev2 = kereszt_nev+vezetek_nev
+print(nev1)
+print(nev2)
+'''
+#2.feladat
+'''
+szam = input('Kérek egy számot:')
+szam1 = szam-1
+szam2 = szam+1
+print(szam1)
+print(szam2)
+'''
+
+#3.feladat
+
+szam1 = int(input('Kérek egy számot:'))
+szam2 = int(input('Kérek egy másik számot:'))
+print(szam1+szam2)
+print(szam1-szam2)
+print(szam1*szam2)
+print(szam1/szam2)
+
